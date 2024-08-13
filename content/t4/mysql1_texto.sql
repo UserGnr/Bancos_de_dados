509dbbc6c35d669f7ee37146d856706feb126d9d
UserGnr <generaldatauser@gmail.com>
2024-08-13 14:31:04 -0300

    Adição da pasta 'content\t4' referente o donwload, instalação e alguns testes

---
+++ content/t4/mysql1_texto.sql
@@ -0,0 +1,47 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mysql1
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema mysql1
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mysql1` DEFAULT CHARACTER SET utf8 ;
+USE `mysql1` ;
+
+-- -----------------------------------------------------
+-- Table `mysql1`.`tipos_produto`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mysql1`.`tipos_produto` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `descricao` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mysql1`.`produtos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mysql1`.`produtos` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `descricao` VARCHAR(45) NOT NULL,
+  `preco` DECIMAL(8,2) NOT NULL,
+  `id_tipo_produto` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_produtos_1_idx` (`id_tipo_produto` ASC) VISIBLE,
+  CONSTRAINT `fk_produtos_1`
+    FOREIGN KEY (`id_tipo_produto`)
+    REFERENCES `mysql1`.`tipos_produto` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
---
+++ content/t4/mysql1_texto2.sql
@@ -0,0 +1,5 @@
+USE mysql1;
+
+SELECT * FROM pacientes;
+
+INSERT INTO tipos_produto (descricao) VALUES ('Notebook');
