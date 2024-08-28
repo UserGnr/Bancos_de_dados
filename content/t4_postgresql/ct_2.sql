d3b260dff53b03c96268d7b4d073cb7df27a16c2
UserGnr <generaldatauser@gmail.com>
2024-08-28 16:10:28 -0300

    Atualizando o arquivo 'content\t4_postgresql\ct_2.sql', adicionado INSERT E SELECT

--- content/t4_postgresql/ct_2.sql
+++ content/t4_postgresql/ct_2.sql
@@ -3,6 +3,7 @@ CREATE DATABASE db_ex1
 -- Não existe o comando USE DATABASE no PostgreSQL
 -- A seleção do banco de dados é feita clicando/selecionando a conexão desejada
 
+-- CREATE
 -- Tipos Produto
 CREATE TABLE tipos_produto(
 	id SERIAL PRIMARY KEY,
@@ -42,3 +43,23 @@ CREATE TABLE turmas(
 	capacidade INT NOT NULL,
 	id_professor INT REFERENCES professores(id) NOT NULL
 );
+
+-- INSERTS
+-- Tipos produtos
+INSERT INTO tipos_produto (descricao) VALUES ('Computadores');
+INSERT INTO tipos_produto (descricao) VALUES ('Impressoras');
+INSERT INTO tipos_produto (descricao) VALUES ('Diversos');
+
+-- Produtos
+INSERT INTO produtos (descricao, preco, id_tipo_produto) VALUES ('Notebook DELL 1544', 2345.67, 1);
+INSERT INTO produtos (descricao, preco, id_tipo_produto) VALUES ('Impr. Jato de Tinta', 456.00, 2);
+INSERT INTO produtos (descricao, preco, id_tipo_produto) VALUES ('Mouse sem fio', 45, 3);
+
+-- Pacientes
+INSERT INTO pacientes (nome, endereco, bairro, cidade, estado, cep, data_nascimento) 
+	VALUES ('Angelina Jolie', 'Rua da paz, 44', 'Nova Lima', 'Santos', 'SP', '121213213', '1978-04-24');
+
+-- SELECT
+SELECT * FROM tipos_produto;
+SELECT * FROM produtos;
+SELECT * FROM pacientes;
