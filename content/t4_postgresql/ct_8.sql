405b68f907709ecff067cd73d5d63776a8dac44f
UserGnr <generaldatauser@gmail.com>
2024-08-31 10:47:46 -0300

    Atualizando o arquivo 'content\t4_postgresql\ct_8.sql' com a utilização do WHERE e do alias(AS)

--- content/t4_postgresql/ct_8.sql
+++ content/t4_postgresql/ct_8.sql
@@ -1,3 +1,4 @@
+-- CREATE
 CREATE DATABASE db_ex7;
 
 CREATE TABLE tipos_produto(
@@ -12,6 +13,7 @@ CREATE TABLE produtos(
 	codigo_tipo INT REFERENCES tipos_produto(codigo) NOT NULL
 );
 
+-- INSERT
 INSERT INTO tipos_produto (descricao) VALUES ('Computador');
 INSERT INTO tipos_produto (descricao) VALUES ('Impressora');
 
@@ -20,6 +22,7 @@ INSERT INTO produtos (descricao, preco, codigo_tipo) VALUES ('Laptop', 1800, 1);
 INSERT INTO produtos (descricao, preco, codigo_tipo) VALUES ('Impr Laser', 500, 2);
 INSERT INTO produtos (descricao, preco, codigo_tipo) VALUES ('Impr. Jato de Tinta', 300, 2);
 
+-- SELECT
 SELECT * FROM tipos_produto WHERE codigo = 1;
 
 SELECT * FROM tipos_produto WHERE codigo = 2;
@@ -28,4 +31,10 @@ SELECT * FROM produtos WHERE descricao = 'Laptop';
 
 SELECT * FROM produtos;
 
+SELECT * FROM tipos_produto;
+
 SELECT * from produtos WHERE preco <= '$500';
+
+SELECT p.codigo, p.descricao, p.preco, tp.descricao AS Tipo
+	FROM produtos AS p, tipos_produto AS tp
+    WHERE p.codigo_tipo = tp.codigo;
